8caa81793971ffcdfd1fdaed8d193bef84b0bc2d
rav1024 <rav1024@gmail.com>
2017-03-02 18:43:55 +0100

    gps.print_data() ok
    only print_data() function is needed in main loop to show data from gps
    unit

--- GPSUnit.cpp
+++ GPSUnit.cpp
@@ -5,3 +5,34 @@ void GPSUnit::get_data(){
       this->encode(this->read());
 }
 
+void GPSUnit::print_data(byte sec){
+  static byte t = 0;
+  static int counter = 0;
+  Serial.begin(9600);
+  String s;
+  
+  this->get_data();
+  
+  if (t != this->time.second())
+    {
+      Serial.print("Lp. ");
+      Serial.println(++counter);
+      Serial.print("Latitude: ");
+      Serial.println(this->location.lat());
+      Serial.print("Longitude: ");
+      Serial.println(this->location.lng());
+      Serial.print("Date: ");
+      Serial.println(this->date.value());
+      Serial.print("Time: ");
+      Serial.println(this->time.value());
+      Serial.print("Satelites: ");
+      Serial.println(this->satellites.value());
+      Serial.println();
+      
+      t = this->time.second();
+      delay((sec-1)*1000);
+    }
+  
+  Serial.end();
+}
+
--- devices_config.ino
+++ devices_config.ino
@@ -1,30 +1,18 @@
 #include "GPSUnit.hpp"
 
-GPSUnit gps(12, 11);
-byte counter = 0;
-byte t = 0;
+const byte gps_rx = 12;
+const byte gps_tx = 11;
+ 
+GPSUnit gps(gps_rx, gps_tx);
 
 void setup() {
   // put your setup code here, to run once:
   gps.whoami();
-  Serial.begin(9600);
 }
 
 void loop() {
 // put your main code here, to run repeatedly:
+       
+    gps.print_data(5);
     
-    gps.get_data();    
-    
-    if (t != gps.time.second())
-    {
-      Serial.println(++counter);
-      Serial.println(gps.location.lat(), 8);
-      Serial.println(gps.location.lng(), 8);
-      Serial.println(gps.date.value());
-      Serial.println(gps.time.value());
-      Serial.println(gps.satellites.value());
-      Serial.println();
-      t = gps.time.second();
-      delay(5010);
-    }
 }

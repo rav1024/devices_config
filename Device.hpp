15443d0b7b6147ad5859a66e091656fc22becbce
rav1024 <rav1024@gmail.com>
2017-03-02 16:39:15 +0100

    GPSUnit class ok
    GPSUnit class works, in main loop you can see readings from gps

---
+++ Device.hpp
@@ -0,0 +1,15 @@
+#ifndef DEVICE_HPP
+#define DEVICE_HPP
+#include <Arduino.h>
+
+class Device{
+protected:
+    String nam;
+    String desc;
+public:
+    Device(String n, String d = ""): nam(n), desc(d) {};
+    void whoami();
+};
+
+#endif // DEVICE_HPP_INCLUDED
+
---
+++ GPSUnit.cpp
@@ -0,0 +1,3 @@
+#include "GPSUnit.hpp"
+
+
